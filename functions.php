93fd2c48f67e144ff392afcf20431e5db6fb9093
Beninasa <rusanovmaxim5@gmail.com>
2026-07-07 14:49:40 +0300

    Задача 1. Подключаем стили правильно
    Добавил основные цвета. Так же подключил в functions.php - стили вызовов для всех объектов

---
+++ functions.php
@@ -0,0 +1,57 @@
+/**
+ * Подключение стилей темы KidHub
+ */
+
+function kidhub_enqueue_styles()
+{
+    wp_enqueue_style(
+        'kidhub-variables',
+        get_template_directory_uri() . '/assets/css/variables.css',
+        [],
+        '1.0'
+    );
+
+    wp_enqueue_style(
+        'kidhub-base',
+        get_template_directory_uri() . '/assets/css/base.css',
+        ['kidhub-variables'],
+        '1.0'
+    );
+
+    wp_enqueue_style(
+        'kidhub-layout',
+        get_template_directory_uri() . '/assets/css/layout.css',
+        ['kidhub-base'],
+        '1.0'
+    );
+
+    wp_enqueue_style(
+        'kidhub-header',
+        get_template_directory_uri() . '/assets/css/header.css',
+        ['kidhub-layout'],
+        '1.0'
+    );
+
+    wp_enqueue_style(
+        'kidhub-footer',
+        get_template_directory_uri() . '/assets/css/footer.css',
+        ['kidhub-header'],
+        '1.0'
+    );
+
+    wp_enqueue_style(
+        'kidhub-buttons',
+        get_template_directory_uri() . '/assets/css/buttons.css',
+        ['kidhub-footer'],
+        '1.0'
+    );
+
+    wp_enqueue_style(
+        'kidhub-hero',
+        get_template_directory_uri() . '/assets/css/hero.css',
+        ['kidhub-buttons'],
+        '1.0'
+    );
+}
+
+add_action('wp_enqueue_scripts', 'kidhub_enqueue_styles');
